febe4679c23c72038fddd8f8a6681f9036c6129c
aschieff <schiefar@clarkson.edu>
2022-11-08 11:54:45 -0500

    Nav Bar

--- js/page.js
+++ js/page.js
@@ -1,4 +1,20 @@
-// a test function that runs when the button is clicked
-function sampleFunction() {
-  alert("You clicked the button");
+// toggles the navigation menu
+function navToggle() {}
+let siteNav = document.getElementById('site-nav');
+let trigram = document.getElementById('trigram');
+if (siteNav.className === 'nav-wrap') {
+  siteNav.classList.add('mobile');
+  trigram.classList.add('tri-mobile');
+} else {
+  siteNav.classList = 'nav-wrap';
+  trigram.classList = 'nav-link';
 }
+// Put slideshow fucntions here
+
+// Put slideshow next and back buttons here
+
+// Put thumbnail image controls here
+
+// Put main slideshow functions here
+
+// Put modal logic here 
